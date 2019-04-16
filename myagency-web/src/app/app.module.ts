9bb4eac44881aa5f2b565a5a645a5bf62e30101b
tobdrue <tobias.drueeke@hs-augsburg.de>
2019-04-16 13:53:48 +0200

    created services and components folders and configured project according

--- myagency-web/src/app/app.module.ts
+++ myagency-web/src/app/app.module.ts
@@ -4,8 +4,8 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LandingPageComponent} from './landing-page/landing-page.component';
-import {TeamMembersComponent} from './landing-page/team-members/team-members.component';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
+import {TeamMembersComponent} from './components/landing-page/team-members/team-members.component';
 import {MatButtonModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
