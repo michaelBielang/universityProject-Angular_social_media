1c93560619c7b02d2a62835608f3c364fceca04f
nrg <michael.bielang@hs-augsburg.de>
2019-04-16 15:26:30 +0200

    37-implement-register add firebase-init

---
+++ myagency-web/src/app/scripts/firebase-init.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/index'
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: 'AIzaSyDW3lgo8KKHpGfEeDrboaadrpiiyHcZCPs',
+  authDomain: 'myagency-9a758.firebaseapp.com',
+  databaseURL: 'https://myagency-9a758.firebaseio.com',
+  projectId: 'myagency-9a758',
+  storageBucket: 'myagency-9a758.appspot.com',
+  messagingSenderId: '620735747060'
+}
+firebase.initializeApp(config)
