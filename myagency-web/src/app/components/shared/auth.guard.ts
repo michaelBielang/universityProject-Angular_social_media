324608edc5ecf0f40306c48bfa3479178e20b2e4
nrg <michael.bielang@hs-augsburg.de>
2019-04-28 12:13:58 +0200

    Merge branch '37-implement-register' to master Includes merge with register
    remove catchphrase

--- myagency-web/src/app/components/shared/auth.guard.ts
+++ myagency-web/src/app/components/shared/auth.guard.ts
@@ -1,10 +1,10 @@
-import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
-import {AuthService} from "../../services/auth.service";
-import {NotifyService} from "../../services/notify.service";
-import {Observable} from "rxjs";
-import "rxjs-compat/add/operator/take";
-import "rxjs-compat/add/operator/do";
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+import {NotifyService} from '../../services/notify.service';
+import {Observable} from 'rxjs';
+import 'rxjs-compat/add/operator/take';
+import 'rxjs-compat/add/operator/do';
 
 
 @Injectable()
@@ -19,12 +19,12 @@ export class AuthGuard implements CanActivate {
 
     return this.authService.user
       .take(1)
-      .map(user => !!(user && user.catchPhrase))
+      .map(user => !!(user))
       .do(loggedIn => {
         if (!loggedIn) {
           this.notify.update('You must be logged in and have a catch phrase!', 'error');
           this.router.navigate(['/login']);
         }
-      })
+      });
   }
 }
