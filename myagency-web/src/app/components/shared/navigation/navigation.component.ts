6b3ed45e6868199808f46ca2dafe48147c21cd6f
nrg <michael.bielang@hs-augsburg.de>
2019-04-30 15:46:07 +0200

    43-search-function
    for discord session 3

---
+++ myagency-web/src/app/components/shared/navigation/navigation.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  theme = '8ecad2';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
