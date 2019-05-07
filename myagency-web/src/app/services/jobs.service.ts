53b9b1899e81ce3ef95c4543121484e51eb45382
tobdrue <tobias.drueeke@hs-augsburg.de>
2019-05-07 10:17:58 +0200

    55 little bug fixes

--- myagency-web/src/app/services/jobs.service.ts
+++ myagency-web/src/app/services/jobs.service.ts
@@ -31,23 +31,13 @@ export enum JobStatus {
 })
 export class JobsService {
 
-  private overview: JobOverview = {
-    imgSrc: 'https://image.pitchbook.com/aVTBCrqHkm8LAvzqdXt8u8ELcJG1476941927661_200x200?uq=H8lJTeVz',
-    client: 'XY',
-    date: '18.06.2019 - 20.06.2019',
-    location: 'Moskva, Russia',
-    fee: '250.000 Rubel (~3460€)',
-    jobId: '123',
-    status: JobStatus.REQUEST
-  };
-
   constructor() {
   }
 
   public jobsOverview(): JobOverview[] {
     const jobOverview = [];
     for (let i = 0; i < 10; i++) {
-      const overCache = this.overview;
+      const overCache = this.createDummyOverview();
       overCache.jobId += i;
       overCache.client += ' ' + i;
       jobOverview.push(overCache);
@@ -62,7 +52,19 @@ export class JobsService {
       detailedLocation: 'XY Factory, Moskva 119121, Russland Ulitsa Plyushchikha, 64',
       jobDetails: 'Fashion shooting with magazin publication and TV commercials for 2 month. The job goes from 18.06.2019 to 20.06.2019',
       photographer: 'Pyotr Tchaikovsky',
-      jobOverview: this.overview
+      jobOverview: this.createDummyOverview()
+    };
+  }
+
+  private createDummyOverview() {
+    return {
+      imgSrc: 'https://image.pitchbook.com/aVTBCrqHkm8LAvzqdXt8u8ELcJG1476941927661_200x200?uq=H8lJTeVz',
+      client: 'XY',
+      date: '18.06.2019 - 20.06.2019',
+      location: 'Moskva, Russia',
+      fee: '250.000 Rubel (~3460€)',
+      jobId: '123',
+      status: JobStatus.REQUEST
     };
   }
 }
